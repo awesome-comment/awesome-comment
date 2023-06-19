6d8e2cf6c504789f9a7eb27b5597757ac5dd6729
Meathill <meathill@gmail.com>
2023-06-19 11:55:33 +0800

    change: daily work

---
+++ packages/awesome-comment/src/components/profile.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue';
+
+const auth0 = useAuth0();
+
+function login() {
+  auth0.loginWithRedirect();
+}
+const {
+  user,
+  isAuthenticated,
+  isLoading,
+} = auth0;
+</script>
+
+<template>
+  <div v-if="isLoading">Loading ...</div>
+  <div v-else>
+    <h2>User Profile</h2>
+    <button @click="login">Log in</button>
+    <pre v-if="isAuthenticated">
+        <code>{{ user }}</code>
+      </pre>
+  </div>
+</template>
--- packages/awesome-comment/src/main.ts
+++ packages/awesome-comment/src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
+import {createAuth0} from "@auth0/auth0-vue";
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(
+  createAuth0({
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
+  })
+);
+app.mount('#app')
