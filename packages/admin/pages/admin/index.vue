f2620dab68e69d5a90f6e899d38f315a6b61c1e6
bamboo <nmz89825@gmail.com>
2023-07-20 11:54:00 +0800

    wip:feat: moderator review comments.

--- packages/admin/pages/admin/index.vue
+++ packages/admin/pages/admin/index.vue
@@ -16,22 +16,22 @@ const comments = computed(() => {
   return data.value.data?.map(c => {
     c.user = JSON.parse((c.user || '{}') as string);
     c.status = Number(c.status);
+    c.id = Number(c.id);
     return c;
   });
 });
 
-const statusText = (status: CommentStatus) => {
-  switch (status) {
-    case CommentStatus.Pending:
-      return 'Pending';
-    case CommentStatus.Approved:
-      return 'Approved';
-    case CommentStatus.Rejected:
-      return 'Rejected';
-    case CommentStatus.Deleted:
-      return 'Deleted';
-  }
+const reviewing = ref(false);
+async function review(id: number, status: CommentStatus) {
+  await useFetch('/api/admin/review', {
+    method: 'POST',
+    body: {
+      id,
+      status,
+    }
+  });
 }
+
 </script>
 
 <template lang="pug">
@@ -40,7 +40,7 @@ main.container.mx-auto.py-8
     h1.text-2xl.font-bold Pending Comments
 
   .loading.loading-ring.loading-lg(v-if="pending")
-  .overflow-x-auto(v-else-if="data.data.length")
+  .overflow-x-auto(v-else-if="data.data?.length")
     table.table.table-pin-rows.table-pin-cols
       thead
         tr
@@ -61,12 +61,30 @@ main.container.mx-auto.py-8
             user-cell(:user="comment.user")
           td {{ comment.created_at }}
           td {{ comment.post_id }}
-          td {{ statusText(comment.status) }}
+          td {{ CommentStatus[comment.status] }}
           th
             .flex.flex-wrap.gap-2(v-if="comment.status === CommentStatus.Pending")
-              button.btn.btn-outline.btn-success.btn-xs Approve
-              button.btn.btn-outline.btn-warning.btn-xs Reject
-              button.btn.btn-outline.btn-error.btn-xs Delete
+              button.btn.btn-outline.btn-success.btn-xs(
+                type="button",
+                :disabled="reviewing",
+                @click="review(comment.id, CommentStatus.Approved)"
+              )
+                span.loading.loading-spinner(v-if="reviewing")
+                | Approve
+              button.btn.btn-outline.btn-warning.btn-xs(
+                type="button",
+                :disabled="reviewing",
+                @click="review(comment.id, CommentStatus.Rejected)"
+              )
+                span.loading.loading-spinner(v-if="reviewing")
+                | Reject
+              button.btn.btn-outline.btn-error.btn-xs(
+                type="button",
+                :disabled="reviewing",
+                @click="review(comment.id, CommentStatus.Deleted)"
+              )
+                span.loading.loading-spinner(v-if="reviewing")
+                | Delete
 </template>
 
 <script lang="ts">
