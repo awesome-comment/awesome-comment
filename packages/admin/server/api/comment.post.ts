ad57d983cca4e7ca8ce20a634dcb53f83fd4f22b
Bamboo <14840170+bambooom@users.noreply.github.com>
2023-08-02 13:15:40 +0800

    refactor: add util to get random tidb API keys. (#8)
    * refactor: add util to get random tidb API keys.
    
    * ver: bumped version
    
    ---------
    
    Co-authored-by: Meathill <meathill@gmail.com>

--- packages/admin/server/api/comment.post.ts
+++ packages/admin/server/api/comment.post.ts
@@ -1,6 +1,7 @@
 import { ResponseBody, User } from '@awesome-comment/core/types';
 import digestFetch, { FetchError } from '@meathill/digest-fetch';
 import { getUser } from '~/utils/api';
+import { getTidbKey } from '~/utils/tidb';
 
 export default defineEventHandler(async function (event): Promise<ResponseBody<number>> {
   const headers = getHeaders(event);
@@ -52,6 +53,7 @@ export default defineEventHandler(async function (event): Promise<ResponseBody<n
   let id: number | null = null;
   try {
     const url = 'https://ap-northeast-1.data.tidbcloud.com/api/v1beta/app/dataapp-NFYbhmOK/endpoint/v1/post';
+    const kv = await getTidbKey();
     const response = await digestFetch(
       url,
       {
@@ -68,11 +70,9 @@ export default defineEventHandler(async function (event): Promise<ResponseBody<n
       {
         method: 'POST',
         realm: 'tidb.cloud',
-        username: process.env.TIDB_PUBLIC_KEY as string,
-        password: process.env.TIDB_PRIVATE_KEY as string,
+        ...kv,
       },
     );
-    console.log('[admin]:', body.comment, body.postId, sub);
     const json = await response.json();
     id = Number(json.data.rows[ 0 ].last_insert_id);
   } catch (e) {
