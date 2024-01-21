72e6107ca185c39a280240ce072d6646e2c7d2dc
Meathill <meathill@gmail.com>
2024-01-21 16:54:30 +0800

    bug: reverted wrongly updated sql

--- tidb/http_endpoints/sql/GET-v3-moderator-uncommented.sql
+++ tidb/http_endpoints/sql/GET-v3-moderator-uncommented.sql
@@ -1,17 +1,17 @@
 USE arealme;
 
-SELECT a.*, b.id as toId, b.content as toContent, b.user as toUser
+SELECT a.*
 FROM ac_comment a
-    LEFT JOIN ac_comment b on a.parent_id = b.id # b is Admin
-WHERE COALESCE(JSON_UNQUOTE(JSON_EXTRACT(b.user, '$.email')), '') IN (${emails})
-    AND a.status IN (0, 1)
-    AND a.deleted_at IS NULL
-    AND IF(LENGTH(${post_id}) > 0, a.post_id = ${post_id}, 1)
-    AND NOT EXISTS ( # not replied by Admin
-        SELECT 1
-        FROM ac_comment c
-        WHERE a.id=c.parent_id
-          AND COALESCE(JSON_UNQUOTE(JSON_EXTRACT(b.user, '$.email')), '') IN (${emails})
-    )
+WHERE (a.parent_id = 0 OR a.parent_id IS NULL)
+  AND COALESCE(JSON_UNQUOTE(JSON_EXTRACT(a.user, '$.email')), '') NOT IN (${emails})
+  AND a.status IN (0, 1)
+  AND a.deleted_at IS NULL
+  AND IF(LENGTH(${post_id}) > 0, a.post_id = ${post_id}, 1)
+  AND NOT EXISTS (
+    SELECT 1
+    FROM ac_comment b
+    WHERE a.id=b.parent_id
+      AND COALESCE(JSON_UNQUOTE(JSON_EXTRACT(b.user, '$.email')), '') IN (${emails})
+  )
 ORDER BY a.id DESC
 LIMIT ${start}, 20;
