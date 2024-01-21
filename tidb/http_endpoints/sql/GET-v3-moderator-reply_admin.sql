8d312d276d1d0677555645ff36000dfc4f29e18c
Meathill <meathill@gmail.com>
2024-01-21 16:57:50 +0800

    bug: fixed `reply_admin` sql

--- tidb/http_endpoints/sql/GET-v3-moderator-reply_admin.sql
+++ tidb/http_endpoints/sql/GET-v3-moderator-reply_admin.sql
@@ -1,16 +1,17 @@
 USE arealme;
 
-SELECT a.*, b.id, b.content, b.ancestor_id
+SELECT a.*, b.id as toId, b.content as toContent, b.user as toUser
 FROM ac_comment a
     LEFT JOIN ac_comment b on a.parent_id = b.id # b is Admin
-    LEFT JOIN ac_comment c on c.parent_id = a.id # c is Users
 WHERE COALESCE(JSON_UNQUOTE(JSON_EXTRACT(b.user, '$.email')), '') IN (${emails})
-    AND (
-        c.id IS NULL
-        OR COALESCE(JSON_UNQUOTE(JSON_EXTRACT(c.user, '$.email')), '') NOT IN (${emails})
-    )
     AND a.status IN (0, 1)
     AND a.deleted_at IS NULL
     AND IF(LENGTH(${post_id}) > 0, a.post_id = ${post_id}, 1)
+    AND NOT EXISTS ( # not replied by Admin
+        SELECT 1
+        FROM ac_comment c
+        WHERE a.id=c.parent_id
+            AND COALESCE(JSON_UNQUOTE(JSON_EXTRACT(c.user, '$.email')), '') IN (${emails})
+    )
 ORDER BY a.id DESC
 LIMIT ${start}, 20;
