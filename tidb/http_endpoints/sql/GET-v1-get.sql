f37a48e10de8e27b79ece476ef5841901ad5b53e
Meathill <meathill@gmail.com>
2024-02-24 22:10:58 +0800

    change: should output 20+1 items for pagination

--- tidb/http_endpoints/sql/GET-v1-get.sql
+++ tidb/http_endpoints/sql/GET-v1-get.sql
@@ -1,35 +1,21 @@
-/* Getting Started:
-Enter "USE {database};" before entering your SQL statements.
-Type "--your question" + Enter to try out AI-generated SQL queries
-Declare a parameter like "Where id = ${arg}".
-*/
 USE arealme;
 
+WITH CTE AS (
+    SELECT id
+    FROM ac_comment
+    WHERE post_id=${post_id}
+      AND status=1
+      AND (
+        ancestor_id IS NULL
+            OR ancestor_id=0
+        )
+    ORDER BY id DESC
+    LIMIT ${start}, 21
+)
 SELECT *
 FROM ac_comment
 WHERE status=1
-  AND id IN(
-	SELECT id 
-	FROM ac_comment
-	WHERE post_id=${post_id}
-	  AND status=1
-      AND (
-		ancestor_id IS NULL
-		OR ancestor_id=0
-	  )
-	ORDER BY id DESC
-	LIMIT ${start}, 20
-)
-  OR ancestor_id IN(
-	SELECT id 
-	FROM ac_comment
-	WHERE post_id=${post_id}
-	  AND status=1
-      AND (
-		ancestor_id IS NULL
-		OR ancestor_id=0
-	  )
-	ORDER BY id DESC
-	LIMIT ${start}, 20
-  )
-ORDER BY id DESC;
+    AND (
+        id IN (SELECT * FROM CTE)
+        OR ancestor_id IN (SELECT * FROM CTE)
+    );
