f8afc16606ec0592244c82d960528c6a74ac94f9
Meathill <meathill@gmail.com>
2024-09-18 23:28:11 +0800

    bug: fixed loading prompts

--- packages/admin/store/prompt.ts
+++ packages/admin/store/prompt.ts
@@ -3,13 +3,13 @@ import type { AiPromptTemplate } from '~/types';
 import { useAuth0 } from '@auth0/auth0-vue';
 
 const usePromptStore = defineStore('prompt', () => {
+  const auth0 = useAuth0();
   const isLoading = ref<boolean>(false);
   const prompts = ref<Record<string, AiPromptTemplate>>({});
 
   // Fetch data from server
   async function refreshPrompts(): Promise<void> {
     isLoading.value = true;
-    const auth0 = useAuth0();
     const token = await auth0.getAccessTokenSilently();
     const { data } = await $fetch<ResponseBody<AiPromptTemplate[]>>('/api/admin/ai-templates', {
       headers: {
