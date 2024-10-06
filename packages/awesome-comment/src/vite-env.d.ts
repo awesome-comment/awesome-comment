b181ea081cf3a18c91df5b4b131ea582232ff871
Meathill <meathill@gmail.com>
2024-10-06 12:00:21 +0800

    change: init auth-admin project

--- packages/awesome-comment/src/vite-env.d.ts
+++ packages/awesome-comment/src/vite-env.d.ts
@@ -5,3 +5,9 @@ declare const __VERSION__: string;
 declare const AwesomeComment: {
   init(dom: string | HTMLElement, apiUrl: string, domain: string, clientId: string): void;
 };
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<unknown, unknown, unknown>;
+  export default component;
+}
