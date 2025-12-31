b94fae5c0bdeb27a95d104a5589c9cb8d79a8e23
Meathill <meathill@gmail.com>
2025-12-31 16:54:52 +0800

    bug: fixed deployment on worker

--- packages/auth-admin-new/vite.config.ts
+++ packages/auth-admin-new/vite.config.ts
@@ -1,7 +1,7 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
-import { defineConfig } from 'vite'
-import ssrPlugin from 'vite-ssr-components/plugin'
+import { cloudflare } from '@cloudflare/vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [cloudflare(), ssrPlugin()]
-})
+  plugins: [cloudflare()],
+});
+
