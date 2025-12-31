0809c27406ea5ffda5acf049aa962093d3389713
Meathill <meathill@gmail.com>
2025-12-31 11:27:05 +0800

    debug: 4

--- packages/auth-admin/server/api/health.ts
+++ packages/auth-admin/server/api/health.ts
@@ -1,11 +1,12 @@
 import { defineEventHandler, type H3Event } from "h3";
 import { getTokenCookieKey, signJwt } from '~/server/utils';
+import pkg from '../../package.json';
 
 export default defineEventHandler(async function (event: H3Event){
   const method = event.node.req.method;
   if (method === 'OPTIONS') {
     console.log('xxx', getTokenCookieKey, signJwt);
-    return '';
+    return pkg.version;
   }
 
   return 'ok';
